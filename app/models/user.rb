d59bc0bd5a0ebd4b8027d12ba944ebe708b3e3b3
github username <github email address>
2020-02-27 17:41:53 -0500

    progress

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ApplicationRecord
     has_many :games
+
+    validates :name, presence: {message: "error: You must create a username."}
+    validates :name, uniqueness: {message: "error: This username is already in use."}
 end
